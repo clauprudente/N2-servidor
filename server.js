39caa5239675794960b58e50df810c5642fedbd3
Claudia <claudiamary.santos@gmail.com>
2019-05-30 22:24:36 -0300

    add metodo post api

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const cors = require('cors')
-const controller = require('./ComidasController.js')
+const bodyParser = require('body-parser')
 
+const controller = require('./ComidasController.js')
 
 const servidor = express()
 servidor.use(cors())
 
 servidor.get("/comidas", (request, response) => {
     response.status(200).send(controller.getAll())
+})
+
+servidor.post("/comidas", bodyParser.json(), (request, response) => {
+    controller.add(request.body)
+    response.sendStatus(201)
 })
 
 servidor.listen(3000, function () {
