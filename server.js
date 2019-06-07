0cc890c8fc10b2622ed4a2f4e56502b89feb4e86
Claudia <claudiamary.santos@gmail.com>
2019-06-07 20:55:39 -0300

    add metodo update server usando express

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const controller = require('./ComidasController.js')
 
 const servidor = express()
 servidor.use(cors())
+servidor.use(bodyParser.json())
 
 servidor.get("/comidas", (request, response) => {
     response.status(200).send(controller.getAll())
 })
 
-servidor.post("/comidas", bodyParser.json(), (request, response) => {
+servidor.post("/comidas", (request, response) => {
     controller.add(request.body)
     response.sendStatus(201)
 })
@@ -19,6 +20,17 @@ servidor.post("/comidas", bodyParser.json(), (request, response) => {
 servidor.delete('/comidas/:id', (request, response) => {
     controller.deleteComida(request.params.id)
     response.sendStatus(204)
+})
+
+servidor.patch('/comidas/:id', (request, response) => {
+    const id = request.params.id;
+    controller.update(id, request.body)
+    response.sendStatus(200)
+})
+
+servidor.put('/comidas/:id', (request, response) => {
+    controller.change(request.params.id, request.body);
+    response.sendStatus(200)
 })
 
 servidor.listen(3000, function () {
